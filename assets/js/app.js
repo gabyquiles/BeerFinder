f5ac81029de1f1e2c766b0dd05ca3c534e07e5e2
Gabriel A. Quiles-Perez <info@gabyquiles.com>
2018-04-07 00:54:23 -0400

    Feature: Add a Map in the front page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Map from './components/Map';
+import Map from './components/MapContainer';
 
 const map = document.getElementById('react-map-root');
 if (map) {
