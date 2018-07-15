9120ae24cf3b64af636d3b1fcb0c96269d60925c
Gabriel A. Quiles-Perez <info@gabyquiles.com>
2018-07-15 13:52:44 -0400

    Feature: Add markers on breweries locations

--- assets/js/components/MapContainer.js
+++ assets/js/components/MapContainer.js
@@ -1,9 +1,6 @@
 import React, {Component} from 'react'
-import {GoogleApiWrapper, Map} from 'google-maps-react'
+import {GoogleApiWrapper, Map, Marker} from 'google-maps-react'
 import {connect} from 'react-redux'
-
-// import Map from './Map'
-
 
 class MapContainer extends Component {
     centerMoved = (mapProps, map) => {
@@ -12,27 +9,31 @@ class MapContainer extends Component {
 
     render() {
         if (!this.props.loaded) {
-            return <div>Loading GAQ...</div>
+            return <div>Loading...</div>
         }
-        const {coords} = this.props
-        console.log(coords)
+        const {coords, breweries} = this.props
         return (
             <Map google={this.props.google} zoom={14} initialCenter={{lat: 0, lng: 0}}
                  center={coords}
                  onDragend={this.centerMoved}>
+                {breweries && breweries.map((brewery) => (
+                    <Marker key={brewery.id} name={brewery.name}
+                            position={{lat: brewery.coordinates.latitude, lng: brewery.coordinates.longitude}}/>
+                ))}
             </Map>
 
         )
     }
 }
 
-function mapStateToProps({location}) {
+function mapStateToProps({location, breweries}) {
     return {
         radius: location.searchRadius,
         coords: {
             lat: location.coordinates ? location.coordinates.latitude : 0,
             lng: location.coordinates ? location.coordinates.longitude : 0,
-        }
+        },
+        breweries: Object.values(breweries)
     }
 }
 
@@ -41,12 +42,3 @@ const ConnectedMapContainer = connect(mapStateToProps)(MapContainer)
 export default GoogleApiWrapper({
     apiKey: "AIzaSyAvjcWfrfm_Z_UvTRteqFLhY41KNSZRtrw"
 })(ConnectedMapContainer)
-
-// <Marker onClick={this.onMarkerClick}
-// name={'Current location'}/>
-//
-// <InfoWindow onClose={this.onInfoWindowClose}>
-//     <div>
-//         <h1>{this.state.selectedPlace.name}</h1>
-//     </div>
-// </InfoWindow>
